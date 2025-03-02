1cbec1d5ac7398835f95172b8841580ef6dddc35
benbel <benjamin.below@softwire.com>
2025-03-02 21:29:42 +0000

    Some additional data collection

--- escape-room-suggestions/src/constants/roomProperties.js
+++ escape-room-suggestions/src/constants/roomProperties.js
@@ -6,7 +6,8 @@ export const RoomProperties = {
   NOVEL: 'novel',
   ACTORS: 'actors',
   PUZZLE_FOCUSED: 'puzzleFocused',
-  TASK_FOCUSED: 'taskFocused'
+  TASK_FOCUSED: 'taskFocused',
+  PHYSICAL: 'physical'
 };
 
 export const RoomPropertyLabels = {
@@ -17,5 +18,6 @@ export const RoomPropertyLabels = {
   [RoomProperties.NOVEL]: 'Novel',
   [RoomProperties.ACTORS]: 'Actors',
   [RoomProperties.PUZZLE_FOCUSED]: 'Puzzle-Focussed',
-  [RoomProperties.TASK_FOCUSED]: 'Task-Focussed'
+  [RoomProperties.TASK_FOCUSED]: 'Task-Focussed',
+  [RoomProperties.PHYSICAL]: 'Physical'
 };
